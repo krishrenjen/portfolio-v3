d9a21c271b9bd1a6a5fcb876ea2a3e179da6aeae
Krish Renjen <krish.renjen@gmail.com>
2025-06-18 10:53:13 -0400

    updated revalidation to post

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { revalidateTag } from 'next/cache'; // ✅ Needed for tag revalidation
 
-export async function GET(req: NextRequest) {
+export async function POST(req: NextRequest) {
   const secret = req.nextUrl.searchParams.get('secret');
   if (secret !== process.env.REVALIDATE_SECRET) {
     return NextResponse.json({ message: 'Invalid token' }, { status: 401 });
